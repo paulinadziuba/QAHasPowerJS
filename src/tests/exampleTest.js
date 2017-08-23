2bb4bf7af6373a9e5a9e719d9a5f27b4d45d68f0
paula <paula.dziuba@gmail.com>
2017-08-23 09:26:03 +0200

    update tests

--- src/tests/exampleTest.js
+++ src/tests/exampleTest.js
@@ -2,6 +2,7 @@ import UserUtils from 'utils/UserUtils';
 
 describe( "open page", (browser) => {
     let UserUtilsObj;
+    let page1;
 
     before((browser, done) => {
         done();
@@ -19,10 +20,10 @@ describe( "open page", (browser) => {
     });
 
     it('should open startup page', (browser) => {
-        browser.url("https://www.google.com/")
+        page1 = browser.page.mainPage();
+        browser.url("http://www.polish.hostelworld.com/")
             .pause(3000);
-        UserUtilsObj = new UserUtils(browser);
-        let randomName = UserUtilsObj.fakeFirstName();
-        console.log(randomName);
+        page1.waitForElementVisible('@homeSearch', 5000);
+        page1.setValue('@homeSearch', 'aaa');
     });
 });
